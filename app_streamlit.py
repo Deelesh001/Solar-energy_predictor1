0b19bbefc8689b3471c0226716822278ffbc3a7d
Deelesh001 <kushalputtyah@gmail.com>
2025-07-11 23:03:01 +0200

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -21,7 +21,7 @@ if not API_KEY:
 
 # ---------------- Streamlit Config ----------------
 st.set_page_config(page_title="Solar Energy Prediction", page_icon="☀️", layout="wide")
-st.image("assets/logo.png", use_container_width=True)
+st.sidebar.image("assets/logo.png", use_column_width=True)
 
 page = st.sidebar.radio("Navigation", ["Home", "About"])
 
